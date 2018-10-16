a95c4aa2c84d7d2db2123082b11a57222e57621e
Jari Selin <jari@selin.fi>
2018-10-16 22:09:30 +0300

    Fixed floating point calculation

--- src/yarn-number.js
+++ src/yarn-number.js
@@ -1,17 +1,18 @@
 
 // NUMBER_VALIDATOR = '(\d+[,.]?\d*){1}[ ]*(?:([xX*/]{1})[ ]*(\d+))?'
 export function parse(str) {
-    return str.replace(',', '.').split(/[xX*\/]/).map( v => parseFloat(v));
+    return str.replace(',', '.').split(/[xX*/]/).map(v => parseFloat(v));
 }
 
 export function multiply(array) {
-    var res = 1;
-    array.map( x => {
-        res *= x;
-    })
+    var res = 1.0;
+
+    for (var i = 0; i < array.length; i++) {
+        res *= array[i];
+    }
     return res;
 }
 
-export function numberingToFloat (str) {
+export function numberingToFloat(str) {
     return multiply(parse(str));
 }
--- src/yarn-number.test.js
+++ src/yarn-number.test.js
@@ -5,6 +5,7 @@ it('multiply', () => {
     expect(multiply([1,2])).toEqual(2);
     expect(multiply([1,2,3])).toEqual(6);
     expect(multiply([4,2])).toEqual(8);
+    expect(multiply([4.1,2.1])).toBeCloseTo(8.61);
 })
 
 it('number to float', () => {
@@ -16,6 +17,6 @@ it('number to float', () => {
     expect(numberingToFloat('1x2x3')).toEqual(6);
     expect(numberingToFloat('1x2x3')).toEqual(6);
     expect(numberingToFloat('1/2/3')).toEqual(6);
-    expect(numberingToFloat('1.0x2.0x3.0')).toEqual(6);
+    expect(numberingToFloat('1.0x2.0x3.0')).toBeCloseTo(6);
     expect(numberingToFloat('1.1/2.2/3.3')).toBeCloseTo(7.986);
 })
