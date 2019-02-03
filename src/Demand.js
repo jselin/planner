3538db587b3fd1282f0e8e393d0a634353dc79be
Jari Selin <jari@selin.fi>
2019-02-03 21:15:47 +0200

    Layout sorta works

--- src/Demand.js
+++ src/Demand.js
@@ -52,6 +52,7 @@ class Demand extends Component {
     e.stopPropagation();
   }
 
+
   render() {
     const callback = (e) => this.handleChange(e);
     const submit = (e) => this.handleSubmit(e);
@@ -59,44 +60,65 @@ class Demand extends Component {
       <div className="Demand">
         <Header header="Demand planner" />
         <div className="Content">
-          <Grid>
-            <Row>
-              <Col>
+          <Form onSubmit={(e) => this.handleSubmit(e)}>
+            <Grid fluid>
+              <Col sm={4}>
                 <DesingInput
                   dimensions={this.state}
                   callback={callback}
                   submit={submit}
                 />
               </Col>
-              <Col>
+              <Col sm={4}>
                 <WeawingInput
                   dimensions={this.state}
                   callback={callback}
                   submit={submit}
                 />
               </Col>
-            </Row>
-            <Row>
-              <Col>
+              <Col sm={4}>
                 <YarnInput
                   dimensions={this.state}
                   callback={callback}
                   submit={submit}
                 />
               </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Result
-                  dimensions={this.state}
-                />
-              </Col>
-            </Row>
-          </Grid>
+            </Grid>
+            <Result
+              dimensions={this.state}
+            />
+          </Form>
         </div>
       </div>
     );
   }
+}
+
+const InputFormatter = (props) => {
+  return (
+    <FormGroup controlId={props.name}>
+      <ControlLabel>{props.label}</ControlLabel>
+      <InputGroup>
+        <FormControl
+          bsSize="small"
+          name={props.name}
+          placeholder={String(props.placeholder)}
+          type="number"
+          onChange={props.callback} />
+        <InputGroup.Addon>{props.unit}</InputGroup.Addon>
+      </InputGroup>
+    </FormGroup>
+  );
+}
+
+const ResultFormatter = (props) => {
+  return (
+    <tr key={props.key}>
+      <td>{props.label}</td>
+      <td>{props.value}</td>
+      <td>{props.unit}</td>
+    </tr>
+  );
 }
 
 const DesingInput = (props) => {
@@ -107,57 +129,55 @@ const DesingInput = (props) => {
   return (
     <div>
       <h1>Design</h1>
-      <Form horizontal onSubmit={(e) => this.handleSubmit(e)}>
-        <InputFormatter
-          name="finished_lenght_m"
-          label="Finished lenght"
-          placeholder={d.finished_lenght_m}
-          unit="m"
-          callback={callback}
-        />
-        <InputFormatter
-          name="headings_hems_lenght_m"
-          label="Headings and hems lenght"
-          placeholder={d.headings_hems_lenght_m}
-          unit="m"
-          callback={callback}
-        />
-        <InputFormatter
-          name="lenght_shrinkage_p"
-          label="Lenght shrinkage"
-          placeholder={d.lenght_shrinkage_p}
-          unit="%"
-          callback={callback}
-        />
-        <InputFormatter
-          name="fring_lenght_m"
-          label="Fringe lenght"
-          placeholder={d.fringe_lenght_m}
-          unit="m"
-          callback={callback}
-        />
-        <InputFormatter
-          name="finished_width_cm"
-          label="Finished width"
-          placeholder={d.finished_width_cm}
-          unit="cm"
-          callback={callback}
-        />
-        <InputFormatter
-          name="width_shrinkage_p"
-          label="Width shrinkage"
-          placeholder={d.width_shrinkage_p}
-          unit="%"
-          callback={callback}
-        />
-        <InputFormatter
-          name="number_of_designs"
-          label="Number of designs"
-          placeholder={d.number_of_designs}
-          unit="n"
-          callback={callback}
-        />
-      </Form>
+      <InputFormatter
+        name="finished_lenght_m"
+        label="Finished lenght"
+        placeholder={d.finished_lenght_m}
+        unit="m"
+        callback={callback}
+      />
+      <InputFormatter
+        name="headings_hems_lenght_m"
+        label="Headings and hems lenght"
+        placeholder={d.headings_hems_lenght_m}
+        unit="m"
+        callback={callback}
+      />
+      <InputFormatter
+        name="lenght_shrinkage_p"
+        label="Lenght shrinkage"
+        placeholder={d.lenght_shrinkage_p}
+        unit="%"
+        callback={callback}
+      />
+      <InputFormatter
+        name="fring_lenght_m"
+        label="Fringe lenght"
+        placeholder={d.fringe_lenght_m}
+        unit="m"
+        callback={callback}
+      />
+      <InputFormatter
+        name="finished_width_cm"
+        label="Finished width"
+        placeholder={d.finished_width_cm}
+        unit="cm"
+        callback={callback}
+      />
+      <InputFormatter
+        name="width_shrinkage_p"
+        label="Width shrinkage"
+        placeholder={d.width_shrinkage_p}
+        unit="%"
+        callback={callback}
+      />
+      <InputFormatter
+        name="number_of_designs"
+        label="Number of designs"
+        placeholder={d.number_of_designs}
+        unit="n"
+        callback={callback}
+      />
     </div>
   );
 }
@@ -170,58 +190,56 @@ const WeawingInput = (props) => {
   return (
     <div>
       <h2>Weaving</h2>
-      <Form horizontal onSubmit={(e) => this.handleSubmit(e)}>
-        <InputFormatter
-          name="test_piece_lenght_m"
-          label="Test piece lenght"
-          placeholder={d.test_piece_lenght_m}
-          unit="m"
-          callback={callback}
-        />
-        <InputFormatter
-          name="number_of_test_pieces"
-          label="Number of test pieces"
-          placeholder={d.number_of_test_pieces}
-          unit="n"
-          callback={callback}
-        />
-        <InputFormatter
-          name="loom_waste_lenght_m"
-          label="Loom waste lenght"
-          placeholder={d.loom_waste_lenght_m}
-          unit="m"
-          callback={callback}
-        />
-        <InputFormatter
-          name="cutting_margin_m"
-          label="Cutting margin"
-          placeholder={d.cutting_margin_m}
-          unit="m"
-          callback={callback}
-        />
-        <InputFormatter
-          name="lenght_take_up_p"
-          label="Lenght take up"
-          placeholder={d.lenght_take_up_p}
-          unit="%"
-          callback={callback}
-        />
-        <InputFormatter
-          name="width_draw_in_p"
-          label="Width draw-in "
-          placeholder={d.width_draw_in_p}
-          unit="%"
-          callback={callback}
-        />
-        <InputFormatter
-          name="selvedge_warps"
-          label="Selvedge warps"
-          placeholder={d.selvedge_warps}
-          unit="n"
-          callback={callback}
-        />
-      </Form>
-    </div>
+      <InputFormatter
+        name="test_piece_lenght_m"
+        label="Test piece lenght"
+        placeholder={d.test_piece_lenght_m}
+        unit="m"
+        callback={callback}
+      />
+      <InputFormatter
+        name="number_of_test_pieces"
+        label="Number of test pieces"
+        placeholder={d.number_of_test_pieces}
+        unit="n"
+        callback={callback}
+      />
+      <InputFormatter
+        name="loom_waste_lenght_m"
+        label="Loom waste lenght"
+        placeholder={d.loom_waste_lenght_m}
+        unit="m"
+        callback={callback}
+      />
+      <InputFormatter
+        name="cutting_margin_m"
+        label="Cutting margin"
+        placeholder={d.cutting_margin_m}
+        unit="m"
+        callback={callback}
+      />
+      <InputFormatter
+        name="lenght_take_up_p"
+        label="Lenght take up"
+        placeholder={d.lenght_take_up_p}
+        unit="%"
+        callback={callback}
+      />
+      <InputFormatter
+        name="width_draw_in_p"
+        label="Width draw-in "
+        placeholder={d.width_draw_in_p}
+        unit="%"
+        callback={callback}
+      />
+      <InputFormatter
+        name="selvedge_warps"
+        label="Selvedge warps"
+        placeholder={d.selvedge_warps}
+        unit="n"
+        callback={callback}
+      />
+    </div >
   );
 }
 
@@ -233,72 +251,44 @@ const YarnInput = (props) => {
   return (
     <div>
       <h1>Yarns</h1>
-      <Form horizontal onSubmit={(e) => this.handleSubmit(e)}>
-        <InputFormatter
-          name="warp_yarn_tex"
-          label="Warp weight"
-          placeholder={d.warp_yarn_tex}
-          unit="TEX"
-          callback={callback}
-        />
-        <InputFormatter
-          name="ends_per_cm"
-          label="Warp thickness"
-          placeholder={d.ends_per_cm}
-          unit="ends / cm"
-          callback={callback}
-        />
-        <InputFormatter
-          name="weft_yarn_tex"
-          label="Weft weight"
-          placeholder={d.weft_yarn_tex}
-          unit="TEX"
-          callback={callback}
-        />
-        <InputFormatter
-          name="picks_per_cm"
-          label="Weft thickness"
-          placeholder={d.picks_per_cm}
-          unit="pics/cm"
-          callback={callback}
-        />
-      </Form>
-    </div>
+      <InputFormatter
+        name="warp_yarn_tex"
+        label="Warp weight"
+        placeholder={d.warp_yarn_tex}
+        unit="TEX"
+        callback={callback}
+      />
+      <InputFormatter
+        name="ends_per_cm"
+        label="Warp thickness"
+        placeholder={d.ends_per_cm}
+        unit="ends / cm"
+        callback={callback}
+      />
+      <InputFormatter
+        name="weft_yarn_tex"
+        label="Weft weight"
+        placeholder={d.weft_yarn_tex}
+        unit="TEX"
+        callback={callback}
+      />
+      <InputFormatter
+        name="picks_per_cm"
+        label="Weft thickness"
+        placeholder={d.picks_per_cm}
+        unit="pics/cm"
+        callback={callback}
+      />
+    </div >
   );
 }
-
-
-
-const InputFormatter = (props) => {
-  return (
-    <FormGroup controlId={props.name}>
-      <Row>
-        <Col sm="2">
-          <ControlLabel>{props.label}</ControlLabel>
-        </Col>
-        <Col sm="2">
-          <InputGroup>
-            <FormControl
-              bsSize="small"
-              name={props.name}
-              placeholder={String(props.placeholder)}
-              type="number"
-              onChange={props.callback} />
-            <InputGroup.Addon>{props.unit}</InputGroup.Addon>
-          </InputGroup>
-        </Col>
-      </Row>
-    </FormGroup>
-  );
-}
-
 
 const Result = (props) => {
   const r = calculateDemand(props.dimensions);
   return (
     <div>
       <h1>Calculated demand</h1>
-      < Table className="Results-Table" bordered hover >
+      <Table className="Results-Table" bordered hover >
         <tbody>
           <ResultFormatter
             key="warp_lenght_m"
@@ -339,16 +329,6 @@ const Result = (props) => {
         </tbody>
       </Table >
     </div>
-  );
-}
-
-const ResultFormatter = (props) => {
-  return (
-    <tr key={props.key}>
-      <td>{props.label}</td>
-      <td>{props.value}</td>
-      <td>{props.unit}</td>
-    </tr>
   );
 }
 
